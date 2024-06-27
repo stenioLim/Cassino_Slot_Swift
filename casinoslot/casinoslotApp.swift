6b7ef94cf7119853c66948e5584aec5a57aaffd2
stenio bomfim <stenio_lima2016@hotmail.com>
2024-06-27 10:06:05 -0300

    Initial Commit

---
+++ casinoslot/casinoslotApp.swift
@@ -0,0 +1,17 @@
+//
+//  casinoslotApp.swift
+//  casinoslot
+//
+//  Created by stenio Lima on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct casinoslotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
